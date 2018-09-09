5ad884488283e62e80833d4a723f4da2c42aaf37
sohil <sohilparikh@gofynd.com>
2018-09-09 19:47:51 +0530

    setting env port and start script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -28,6 +29,6 @@ app.get('/about', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server is up')
 });
